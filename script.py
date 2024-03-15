ecae2c4162e3d7c15325dd7c6de86d05b6b1f7b8
yjyolandeyan <158221697+yjyolandeyan@users.noreply.github.com>
2024-03-15 19:37:23 -0400

    Update script.py

--- script.py
+++ script.py
@@ -63,8 +63,7 @@ def scrape_data_point():
                 headlines['Opinion'] = opinion_headline_element.get_text(strip=True)
 
         # Scrape the multimedia headline
-        multimedia_headline_element = soup.find("a", class_="medium-link", href="/multimedia/")
-        headlines['Opinion'] = "" if multimedia_headline_element is None else multimedia_headline_element.text
+
         
         loguru.logger.info(f"Scraped headlines: {headlines}")
     return headlines
