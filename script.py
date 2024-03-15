adf44f6068094b405beaaa7a7d6c1ac23c571236
yjyolandeyan <158221697+yjyolandeyan@users.noreply.github.com>
2024-03-15 19:10:29 -0400

    Update script.py

--- script.py
+++ script.py
@@ -31,44 +31,34 @@ def scrape_data_point():
     #     loguru.logger.info(f"Data point: {data_point}")
     #     return data_point
 
-    # req = requests.get("https://www.thedp.com")
-    # loguru.logger.info(f"Request URL: {req.url}")
-    # loguru.logger.info(f"Request status code: {req.status_code}")
-
-    # if req.ok:
-    #     soup = bs4.BeautifulSoup(req.text, "html.parser")
-    #     target_element = soup.find("a", class_=["frontpage-link medium-link newstop"])
-    #     headline = "" if target_element is None else target_element.text.strip()
-    #     loguru.logger.info(f"Headline: {headline}")
-    #     return f"Top headline from News is: {headline}"
-
-    headlines = {'main': '', 'news': '', 'sports': ''}
     req = requests.get("https://www.thedp.com")
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
-
+    headlines = {'main': '', 'news': '', 'sports': ''}
+    
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-
         # Scrape the main headline
         main_headline_element = soup.find("a", class_="frontpage-link")
         if main_headline_element:
             headlines['main'] = main_headline_element.get_text(strip=True)
-
         # Scrape the news headline
-        news_headline_element = soup.find("div", class_="section-news").find("a", class_="frontpage-link")
-        if news_headline_element:
-            headlines['news'] = news_headline_element.get_text(strip=True)
+            news_headline_element = soup.select_one("div.col-sm-6.section-news a.frontpage-link")
+            if news_headline_element:
+                headlines['news'] = news_headline_element.get_text(strip=True)
 
-        # Scrape the sports headline
-        sports_headline_element = soup.find("div", class_="section-sports").find("a", class_="frontpage-link")
-        if sports_headline_element:
-            headlines['sports'] = sports_headline_element.get_text(strip=True)
+            # Scrape the sports headline
+            sports_headline_element = soup.select_one("div.article-summary a.frontpage-link")
+            if sports_headline_element:
+                headlines['sports'] = sports_headline_element.get_text(strip=True)
 
-        loguru.logger.info(f"Headlines: {headlines}")
-            
-        return headlines
+            loguru.logger.info(f"Scraped headlines: {headlines}")
 
+        # target_element = soup.find("a", class_=["frontpage-link medium-link newstop"])
+        # headline = "" if target_element is None else target_element.text.strip()
+        # loguru.logger.info(f"Headline: {headline}")
+        # return f"Top headline from News is: {headline}"
+    return headlines
 
 if __name__ == "__main__":
 
